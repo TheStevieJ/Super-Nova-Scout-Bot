df22ddb8caae0f77203764bbe7500a6f54ae9bda
Stephen Johnson <3695StephenJohnson@gmail.com>
2019-12-16 15:09:02 -0800

    Add google

---
+++ google.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+SAMPLE_SPREADSHEET_ID = '1_HvcUu2ocIeDIk-xJ76zIPBghYg-K-SzuqjYyrkeSd8'
+SAMPLE_RANGE_NAME = 'Output!A:A'
+
+def main():
+	creds = None
+	if os.path.exists('token.pickle'):
+		with open('token.pickle', 'rb') as token:
+			creds = pickle.load(token)
+
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file(
+				'credentials.json', SCOPES)
+			creds = flow.run_local_server(port=0)
+
+		with open('token.pickle', 'wb') as token:
+			pickle.dump(creds, token)
+	service = build('sheets', 'v4', credentials=creds)
+	sheet = service.spreadsheets()
+	result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+								range=SAMPLE_RANGE_NAME).execute()
+	values = result.get('values', [])
+
+	if not values:
+		print('No data found.')
+	else:
+		print('Name, Major:')
+		print(values)
+
+main()
--- update.py
+++ update.py
@@ -1,21 +1,32 @@
 import requests
+from database import connect_db
 
+def get_dragon(url):
+	r = requests.get(url,{})
+	data = r.json()
+	return data
 
 def update_champions():
+	data = get_dragon("http://ddragon.leagueoflegends.com/cdn/9.23.1/data/en_US/champion.json")
+	champs = list(data["data"].keys())
+	out_data = []
+	for champ in champs:
+		name = data["data"][champ]["id"]
+		num = data["data"][champ]["key"]
+		out_data.append((num,name))
+	#print(out_data)
+	db = connect_db()
+	cursor = db.cursor()
+	sql = "INSERT INTO champs (champId, name) VALUES (%s, %s)"
+	cursor.executemany(sql, out_data)
+	db.commit()
 	return
 
 def update_spells():
 	return
 
 def update_items():
-	url = "http://ddragon.leagueoflegends.com/cdn/9.23.1/data/en_US/item.json"
-	r = requests.get(url,{})
-	data = r.json()
-	items = []
-	for item in list(data["data"].keys()):
-		temp = (item, data["data"][item]["name"])
-		items.append(temp)
-
+	return
 
 def update_runes():
 	return
@@ -27,4 +38,4 @@ def patch():
 	update_runes()
 	return
 
-update_items()
+update_champions()
